858e4bde54eabc333760521df0b8b6dd569d250e
dariusjonda <mail@dariusjonda.de>
2021-09-16 18:06:19 +0200

    Update dwmblocks title to improve visibility
    battery status in the past showed only a percentage number
    which lead to mistaking it for volume level.
    GPU / CPU updated for consistency reasons.

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,12 +3,12 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "curl -s ipinfo.io | sed -n '2p;4p' | grep -e city -e ip |  awk '{print $2}' |tr -s '\n' ' ' | sed 's/\", \"/ /g' | sed 's/\"//g' | sed 's/,//g' | sed 's/.$//g'",	30,		0},
 	{"", "curl -s wttr.in/Berlin?format=1", 30, 0},
-	{"", "db_battery", 30, 0},
+	{"BAT ", "db_battery", 30, 0},
   {"", "upower -i $(upower -e | grep 'BAT') | grep energy-rate | awk '{print $2 $3}'", 30, 0}, 
 	{"", "db_volume", 1, 0},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"G:", "sensors | grep edge | awk '{print $2}'",	5,		0},
-	{"C:", "sensors | grep Tctl | awk '{print $2}'",	5,		0},
+	{"GPU ", "sensors | grep edge | awk '{print $2}'",	5,		0},
+	{"CPU ", "sensors | grep Tctl | awk '{print $2}'",	5,		0},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
