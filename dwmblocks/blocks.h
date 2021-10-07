64652655babfa0f44eaa3c49ff5eafd4f6fcea77
dariusjonda <mail@dariusjonda.de>
2021-10-07 19:22:11 +0200

    Add st patch to display ligatures

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "curl -s ipinfo.io | sed -n '2p;4p' | grep -e city -e ip |  awk '{print $2}' |tr -s '\n' ' ' | sed 's/\", \"/ /g' | sed 's/\"//g' | sed 's/,//g' | sed 's/.$//g'",	30,		0},
 	{"", "curl -s wttr.in/Berlin?format=1", 30, 0},
-	{"BAT ", "db_battery", 30, 0},
+	{"⚡", "db_battery", 30, 0},
   {"", "upower -i $(upower -e | grep 'BAT') | grep energy-rate | awk '{print $2 $3}'", 30, 0}, 
 	{"", "db_volume", 1, 0},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
